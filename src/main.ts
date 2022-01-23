75e4e5c38e131b25839618ed4849c478a16ab2be
monthebrice2000 <65128522+monthebrice2000@users.noreply.github.com>
2022-01-23 12:10:40 +0100

    set port environement variable of the app

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const port = process.env.port_app;
   app.useGlobalPipes(new ValidationPipe());
-  await app.listen(port);
+  await app.listen(parseInt(process.env.PORT) || port);
   Logger.log(`Application running on port ${port}`);
 }
 bootstrap();
